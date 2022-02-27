571452290dbd5d22cbfaac390ba946799966a7b9
Touhidul Shawan <touhidulshawan@gmail.com>
2022-02-27 15:31:23 +0600

    add switch case & enum

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -15,6 +15,7 @@ void greeting(String name) {
   print("Welcome to Dart World, $name");
 }
 
+enum Operation { plus, minus, multiply, divide }
 // take user input
 
 void takeUserInput() {
@@ -26,13 +27,61 @@ void takeUserInput() {
   print("Enter 2nd number: ");
   int secondNumber = int.parse(stdin.readLineSync()!);
 
-  addition(firstNumber, secondNumber);
+  calculate(firstNumber, secondNumber, Operation.plus);
 }
 
-// make addition
+// addition
 
-void addition(int firstNumber, int secondNumber) {
-  int result = firstNumber + secondNumber;
+void addition(int num1, int num2) {
+  var result = num1 + num2;
+  print("Sum : $result");
+}
 
-  print("Sum of $firstNumber + $secondNumber = $result");
+// do subtraction
+void subtraction(int num1, int num2) {
+  var result;
+
+  if (num1 > num2) {
+    result = num1 - num2;
+  } else {
+    result = num2 - num1;
+  }
+  print("Subtraction: $result");
+}
+
+// do multiplication
+
+void multiply(int num1, int num2) {
+  var result = num1 * num2;
+  print("Multiplication: $result");
+}
+
+// do division
+
+void division(int num1, int num2) {
+  var result = (num1 / num2).round();
+
+  print("Division: $result");
+}
+
+// calculate
+
+void calculate(int num1, int num2, Operation op) {
+  switch (op) {
+    case Operation.plus:
+      addition(num1, num2);
+      break;
+    case Operation.minus:
+      subtraction(num1, num2);
+      break;
+    case Operation.multiply:
+      multiply(num1, num2);
+      break;
+    case Operation.divide:
+      division(num1, num2);
+      break;
+    default:
+      print("Invalid Choice");
+      break;
+  }
 }

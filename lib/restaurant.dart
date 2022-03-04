706805e9c84f85821a6ea3bcbcb8967ac381cbfd
Touhidul Shawan <touhidulshawan@gmail.com>
2022-03-04 20:00:18 +0600

    change restaurant file to class

--- lib/restaurant.dart
+++ lib/restaurant.dart
@@ -1,33 +1,43 @@
-void restaurant() {
-  var restaurants = [
-    {
-      'name': 'Pizza Mario',
-      'cuisine': 'Italian',
-      'ratings': [4.0, 3.5, 4.5],
-    },
-    {
-      'name': 'Chez Anne',
-      'cuisine': 'French',
-      'ratings': [5.0, 4.5, 4.0],
-    },
-    {
-      'name': 'Navaratna',
-      'cuisine': 'Indian',
-      'ratings': [4.0, 4.5, 4.0],
-    },
-  ];
+class Restaurant {
+  const Restaurant({
+    required this.name,
+    required this.cuisine,
+    required this.ratings,
+  });
 
-  for (var restaurant in restaurants) {
-    final ratings = restaurant['ratings'] as List<double>;
+  final String name;
+  final String cuisine;
+  final List<double> ratings;
 
-    var total = 0.0;
-    for (var rating in ratings) {
-    total += rating;
+  int get numRatings => ratings.length;
+  String? avgRating() {
+    if (ratings.isEmpty) {
+      return null;
     }
-    final avgRating = total / ratings.length;
-
-    restaurant['avgRating'] = avgRating.round();
+    return (ratings.reduce((value, element) => value + element) /
+            ratings.length)
+        .toStringAsFixed(2);
   }
 
-    print(restaurants);
+  void getInfo() {
+    print("Restaurant: $name | Cuisine: $cuisine");
+  }
+}
+
+void restaurant() {
+  final rest1 = Restaurant(
+      name: 'Pizza Mario', cuisine: 'Italian', ratings: [4.0, 3.5, 4.5]);
+  final rest2 = Restaurant(
+      name: 'Chez Anne', cuisine: 'French', ratings: [5.0, 4.5, 4.0]);
+  final rest3 = Restaurant(
+      name: 'Navaratna', cuisine: 'Indian', ratings: [4.0, 4.5, 4.0]);
+
+  rest1.getInfo();
+  print('Average Rating of ${rest1.name} : ${rest1.avgRating()}');
+
+  rest2.getInfo();
+  print('Average Rating of ${rest2.name} : ${rest2.avgRating()}');
+
+  rest3.getInfo();
+  print('Average Rating of ${rest3.name} : ${rest3.avgRating()}');
 }
